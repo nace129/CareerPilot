0c0a24eca015c6c8503666c65a26bae45c36f851
Nidhi <nidhi.b.shah@sjsu.edu>
2025-04-16 13:12:57 -0700

    Fix: Security group added to EC2 instance to allow SSH

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,31 @@ resource "aws_instance" "flask_server" {
 
   user_data = file("${path.module}/deploy_backend.sh")
 
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
   tags = {
     Name = "CareerPilot-Backend"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+#   vpc_id      = "vpc-xxxxxxxx"  # Optional if using default VPC
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ Open to all for now
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
